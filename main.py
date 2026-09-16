5b33f1c61df56563a542cf2e89e774e4cbbae22d
sam <samuel.e.achilike@gmail.com>
2026-09-16 20:03:39 +0100

    Revert "Stage 4: full CRUD"
    This reverts commit c0ee359525e82bbbd40b4e68ef119b35f5b4d66d.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI, HTTPException, status
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Task(BaseModel):
     id: int
-    name: str = Field(min_length=1)
-    done: bool
+    name: str
+    done: float
 
-class UserTask(BaseModel):
-    name: str = Field(min_length=1)
-    done: bool = False
-
-total_tasks = [
+complete_tasks = [
     Task(id = 1, name= "Laundry", done = True),
     Task(id= 2, name= "Market errands", done= True),
     Task(id= 3, name= "Cooking", done= True),
@@ -31,12 +28,12 @@ async def root():
 
 @app.get("/tasks", response_model=list[Task])
 async def get_tasks():
-    return total_tasks
+    return complete_tasks
 
 @app.get("/tasks/{id}", response_model=Task)
 async def get_task(id: int):
 
-    for task in total_tasks:
+    for task in complete_tasks:
         if task.id == id:
             return task
 
@@ -44,17 +41,3 @@ async def get_task(id: int):
         status_code= status.HTTP_404_NOT_FOUND,
         detail= f"The ID: {id} was not found."
     )
-
-@app.post("/tasks", status_code= status.HTTP_201_CREATED)
-async def create_task(task: UserTask):
-    task_id: int = len(total_tasks) + 1
-    task_name = task.name
-    done = task.done
-
-    user_task = Task(id= task_id,
-                     name= task_name,
-                     done= done)
-    
-    total_tasks.append(user_task.model_dump())
-
-    return user_task
